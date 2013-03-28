75b99f1ebf7124ffe167dab6267dc11b2e96bfd6
unknown <Robert>
2013-03-28 00:20:14 -0700

    This is the 2nd commit

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-test
+function json_array_filter(data,type){
+    
+    //check if data is set
+    if (typeof data != 'undefined') { return false;}
+    //check if type is set
+    if (typeof type != 'undefined') { type = 'json';}
+    
+    if(type == 'json'){ obj = eval('(' + jsonObject + ')'); }else{obj = data;}
+    
+    
+    
+}
