ae82aa619c8af7e7b6b43342cdf978cef77d0f36
Fujio Turner <Fujio Turner@Fujio>
2013-03-28 00:52:34 -0700

    from a server 3-20-13 part local machine

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-function json_array_filter(data,type){
+function json_array_filter(data,dataType){
     
     //check if data is set
-    if (typeof data != 'undefined') { return false;}
+    if (typeof data == 'undefined') { return false;}
     //check if type is set
-    if (typeof type != 'undefined') { type = 'json';}
+    if (typeof dataType == 'undefined') { dataType = 'object';}else{ dataType = 'json';}
     
-    if(type == 'json'){ obj = eval('(' + jsonObject + ')'); }else{obj = data;}
-    
-    
-    
+    if(dataType == 'json'){ var obj = eval('(' + data + ')'); }else{var obj = data;}
+     
 }
