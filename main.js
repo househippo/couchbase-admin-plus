8c2a9b90a893ee37a1e6e6b7d6035b036c766318
Fujio Turner <Fujio Turner@Fujio>
2013-04-04 18:01:14 -0700

    4-04-13 End of Day

--- main.js
+++ main.js
@@ -1,12 +1,77 @@
-function json_array_filter(data,dataType){
+//I want to pass an object array into this filter and sort an mulidimentional array of objects parsed from a json string.
+function filterObject(obj,branch,sorter,range,find) {
     
-    //check if data is set
-    if (typeof data == 'undefined') { return false;}
-    //check if type is set
-    if (typeof dataType == 'undefined') { dataType = 'object';}else{ dataType = 'json';}
-    
-    if(dataType == 'json'){ var obj = eval('(' + data + ')'); }else{var obj = data;}
-    
-    //test 2
+    /*
+     * the "obj" is an array of js objects
+     * the "branch" is the location in the MDA(Multi Dimentional Array)
+     * EX. obj.BookReviews.NameOfReviewer .... I want to pass "BookReviews.NameofReviewr" so that function know how to filter the obj
+     * the "sorter" is 
+     * the "range" is the method of sort 'AtoZ'... LtoH ... 
+     * 
+     * 
+    */
      
+     var SortObj = false;
+    if(typeof    obj == 'undefined' || typeof obj != 'object'){return false;}  // make sure it a JS object
+    if(typeof branch == 'undefined' || branch.isArray == false){return false;} // make sure
+    if(typeof  sorter == 'undefined'){   }
+    if(typeof  range == 'undefined'){  range = 'AtoZ';  }
+    
+    
+    switch(sorter){       
+      
+      case"AtoZ":
+      case"ZtoA":
+      case"LtoH": 
+      case"HtoL":
+      SortObj = true;
+      break; 
+      
+      default:
+      break;
+    }
+    
+    if(range == 'AtoZ'){
+        
+        this.AtoZ = function() {
+         
+            /*
+            $.each(obj,function(index,value){            
+            console.log(index + ' | ' + value[branch[0]]);   
+            })
+            */
+           // here comes the magic that allows chaining:
+            //return objInner;
+            //return this;
+        }
+    }
+    
+    
+    if(range == 'ZtoA'){
+        this.ZtoA = function() {
+
+            // here comes the magic that allows chaining:
+            return this;
+        }
+    }
+    if(range == 'LtoH'){  
+        this.LtoH = function() {
+
+            // here comes the magic that allows chaining:
+            return this;
+        }
+    }
+    if(range == 'HtoL'){
+        this.HtoL = function() {
+
+            // here comes the magic that allows chaining:
+            return this;
+        }
+    }
+    
+    // this.two analogous
+    this.Back = function() {
+  
+    return obj;
+    }
 }
